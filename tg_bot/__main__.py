15cd38452a9ea19d9231a8a022b4190da4f387f0
deon378 <70142311+deon378@users.noreply.github.com>
2020-11-18 20:19:41 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,9 +19,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {},My Name is {} !. 
-
-I'm Filter Manager Bot Maintained By [{}](https://t.me/{}). 
+I am specially made for [VELLITHIRAS](t.me/vellithiras) group.
 
 """
 
